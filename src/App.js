335e90a1ddbcd9af30ad0223986101695bae9fa4
makerunleashed <82834016+makerunleashed@users.noreply.github.com>
2024-02-02 17:52:17 +0000

    New files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import Header from './header';
+import Footer from './footer';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="main-content">
+        <div className="left-column">
+          Left Column
+        </div>
+        <div className="center-column">
+          Center Column
+        </div>
+        <div className="right-column">
+          Right Column
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <h3>This is the Footer</h3>
+    </footer>
+  );
+}
+
+export default Footer;
